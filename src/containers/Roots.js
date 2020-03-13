9a3d9bf03ff6484ba0dada5c98ffe0a17c566bad
Vinesh Pendyala <vinesh.r.pendyala@gmail.com>
2020-03-13 11:01:48 -0400

    Fixing errors in progress

---
+++ src/containers/Roots.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { Provider } from 'react-redux'
+
+import configureStore from '../configureStore'
+import App from './App'
+
+const store = configureStore()
+
+// Base of the app
+export default class Root extends Component {
+    render() {
+        return (
+            <Provider store={store}>
+                <App />
+            </Provider>
+        )
+    }
+}
